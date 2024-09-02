ca9783961c61035d30126b3c7baa06b8fec0a8ea
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-09-02 10:21:24 +0300

    Update leet7.py
    udpated with the 2nd optimal solution by using K dense algorithm

--- leet7.py
+++ leet7.py
@@ -13,4 +13,16 @@ class Solution:
                 max_sum=max(max_sum,curr_sum)
         return max_sum          
 
+# optimal solution by using K Dense Algorithm        
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = -10**9
+        curr_sum = 0
+        for values in nums:
+          if curr_sum < 0:
+             curr_sum=0
+          curr_sum+=values
+          max_sum = max(max_sum, curr_sum)
+        return max_sum     
+
         
